d4d32a30ca9e5e0ab16082328acecdd211ead39e
Freewind <nowindlee@gmail.com>
2021-01-18 14:23:58 +0800

    typescript-react-mui-datatables-string-array-row-demo

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-TypeScript React "material-ui" Demo
+TypeScript React "material-ui" String Array Row Demo
 ===================================
 
-如何使用TypeScript来写"material-ui"
+基于material-ui的开源数据表。简单情况下可以使用字符串数组作为数据
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,33 +1,18 @@
-import React, {useState} from 'react'
+import React from 'react'
+import MUIDataTable from "mui-datatables";
 
-import {List, ListItem, ListItemText, ListItemIcon, Collapse} from '@material-ui/core'
-import InboxIcon from '@material-ui/icons/Inbox'
-import {ExpandMore, ExpandLess} from '@material-ui/icons'
-
-export default function MyList() {
-  const [open, setOpen] = useState(false)
-
-  return <div>
-    <List>
-      <ListItem button onClick={() => setOpen(!open)}>
-        <ListItemIcon>
-          <InboxIcon/>
-        </ListItemIcon>
-        <ListItemText primary='Hello'/>
-        {
-          open ? <ExpandLess/> : <ExpandMore/>
-        }
-      </ListItem>
-      <Collapse in={open}>
-        <List>
-          <ListItem>
-            <ListItemText primary='typescript'/>
-          </ListItem>
-          <ListItem>
-            <ListItemText primary='material-ui'/>
-          </ListItem>
-        </List>
-      </Collapse>
-    </List>
-  </div>
+export default function Hello() {
+  return <MUIDataTable
+    title={"Employee List"}
+    data={[
+      ["Joe James", "Test Corp", "Yonkers", "NY"],
+      ["John Walsh", "Test Corp", "Hartford", "CT"],
+      ["Bob Herm", "Test Corp", "Tampa", "FL"],
+      ["James Houston", "Test Corp", "Dallas", "TX"],
+    ]}
+    columns={["Name", "Company", "City", "State"]}
+    options={{
+      filterType: 'checkbox',
+    }}
+  />
 }
